cdb85ea120fcbdcb4b16822a52aaadca8352979b
donypramujo <donypramujo@gmail.com>
2016-03-21 09:18:55 +0700

    Add assets

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,39 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.styles([
+        'bootstrap.css',
+        'animate.css',
+        'font-awesome.min.css',
+        'font.css',
+//        '../js/calendar/bootstrap_calendar.css',
+        'app.css'
+    ]);
 });
+
+elixir(function(mix) {
+    mix.scripts([
+        'jquery.min.js',
+        'bootstrap.js',
+        'app.js',
+        'slimscroll/jquery.slimscroll.min.js',
+        'charts/easypiechart/jquery.easy-pie-chart.js',
+        'charts/sparkline/jquery.sparkline.min.js',
+        'charts/flot/jquery.flot.min.js',
+        'charts/flot/jquery.flot.tooltip.min.js',
+        'charts/flot/jquery.flot.resize.js',
+        'charts/flot/jquery.flot.grow.js',
+//        'calendar/bootstrap_calendar.js',
+//        'calendar/demo.js',
+        'sortable/jquery.sortable.js',
+        'app.plugin.js'
+    ]);
+});
+
+elixir(function(mix) {
+    mix.scripts([
+         'ie/html5shiv.js', 
+         'ie/respond.min.js',
+         'ie/excanvas.js'],
+         'public/js/ie-plugin.js');
+	});
